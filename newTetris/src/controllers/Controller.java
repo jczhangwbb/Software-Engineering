49d93fd5c2f17fbcc1c36b3a4d7ed89c1717195b
DangoPlus <depengyu@gmail.com>
2018-01-11 00:22:43 +0800

    update tetris status

--- newTetris/src/controllers/Controller.java
+++ newTetris/src/controllers/Controller.java
@@ -45,4 +45,66 @@ public class Controller {
 		clearAll();
 	}
 	
+	private boolean tryMove(PieceType newPiece, int newX, int newY) {
+		for (int i = 0; i < 4; ++i) {
+			int x = newX + newPiece.x(i);
+			int y = newY - newPiece.y(i);
+			if (x < 0 || x >= penalWidth || y < 0 || y >= penalHeight)
+				return false;
+			if (PieceAt(x, y) != PieceType.AllType.Empty)
+				return false;
+		}
+		//this.nowpiece = newPiece;
+		//this.nowX = newX;
+		//this.nowY = newY;
+		nowPiece = newPiece;
+		nowX = newX;
+		nowY = newY;
+		gameBoard.repaint();
+		return true;
+	}
+	public void goLeft() { // 向左移動方塊的方法
+		tryMove(nowPiece, nowX - 1, nowY);
+	}
+	public void goRight() { // 向右移動方塊的方法
+		tryMove(nowPiece, nowX + 1, nowY);
+	}
+	public void rotate() { // 旋轉方塊的方法
+		tryMove(nowPiece.rotate(), nowX, nowY);
+	}
+	public void gameAction() {
+		if (isReachBottom) {
+			isReachBottom = false;
+			newPiece();
+		} else {
+			speedUp();
+		}
+	}
+	public boolean isStarted() {
+		return isRunning;
+	}
+	public boolean isPaused() {
+		return isStoped;
+	}
+	public boolean isCurrentPieceEmpty() {
+		return nowPiece.getPieceShape() == PieceType.AllType.Empty;
+	}
+	public void start() {
+		if (isStoped)
+			return;
+		isRunning = true;
+		isReachBottom = false;
+		score = 0;
+		clearAll();
+		newPiece();
+		timer.start();
+	}
+	
+//	public void newGame()
+//{
+//	gameBoard = new GameBoadrd(this);
+//	//System.out.println("newgame"+ score);
+//	gameBoard.start;
+//}
+	
 }
