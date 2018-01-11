10e89509de184eac1018eb665295872632f60fba
赵梓丞 <a10615001@mail.ntust.edu.tw>
2018-01-12 01:09:43 +0800

    A10615001 view

---
+++ newTetris/src/A10615001/Frame.java
@@ -0,0 +1,98 @@
+package A10615001;
+
+import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+import A10615001.Frame;
+import controllers.Controller;
+
+import java.awt.*;
+
+public class Frame extends JFrame {
+	// 声明按钮 开始 、暂停
+	private JButton bStart;
+	private JButton bPause;
+	
+	private JLabel statusBar;
+	private GamePenal gameBoard;
+	private JPanel sidePanel;
+	private JPanel container;
+	private Controller controller;
+	private JTextArea howToPlay;
+
+	public Frame() {
+		
+		
+
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 400);
+
+		statusBar = new JLabel("Gaming");
+		howToPlay = new JTextArea();
+		howToPlay.append(" ⬅ : Move Left \r\n ➡ : Move Right \r\n ⬆ : Rotate \r\n ⬇ : dropDown \r\n Space: Speed up");
+		howToPlay.setEditable(false);
+		
+		bStart= new JButton("开始");
+		bPause= new JButton("暂停");
+		gameBoard = new GamePenal(this);
+
+		container = new JPanel();
+
+		setContentPane(container);
+
+		sidePanel = new JPanel();
+			
+
+		GroupLayout glcontainer = new GroupLayout(container);
+		glcontainer.setHorizontalGroup(glcontainer.createParallelGroup(Alignment.LEADING)
+				.addGroup(glcontainer.createSequentialGroup().addContainerGap()
+				.addComponent(gameBoard, GroupLayout.PREFERRED_SIZE, 240, GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(ComponentPlacement.RELATED)
+				.addComponent(sidePanel, GroupLayout.DEFAULT_SIZE, 120, Short.MAX_VALUE)));
+		glcontainer.setVerticalGroup(glcontainer.createParallelGroup(Alignment.LEADING)
+				.addGroup(glcontainer.createSequentialGroup()
+				.addGroup(glcontainer.createParallelGroup(Alignment.LEADING)
+				.addComponent(sidePanel, GroupLayout.DEFAULT_SIZE, 500, Short.MAX_VALUE) 
+				.addGroup(glcontainer.createSequentialGroup().addGap(6)
+				.addComponent(gameBoard,GroupLayout.DEFAULT_SIZE, 500, Short.MAX_VALUE))) 
+				.addContainerGap()));
+		sidePanel.setLayout(new BoxLayout(sidePanel,1));
+		sidePanel.add(bStart);
+		sidePanel.add(bPause);
+		sidePanel.add(howToPlay);
+		sidePanel.setBackground(Color.LIGHT_GRAY);
+		container.setLayout(glcontainer);
+
+	}
+
+	public void init() {
+		setTitle("決戰俄羅斯");
+		gameBoard.start();
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		pack();
+		setVisible(true);
+		setResizable(false);
+	}
+
+	JLabel getStatusBar() {
+		return statusBar;
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame game = new Frame();
+					game.setVisible(true);
+					game.setLocationRelativeTo(null);
+					game.init();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
